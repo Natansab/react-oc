19c06dd2b37db20336da1f71aaaca3c66ac3a037
Natan Sabbah <nsabbah@student.42.fr>
2017-09-04 14:43:12 +0200

    sort by name ok

--- product_list/src/components/ProductTable.js
+++ product_list/src/components/ProductTable.js
@@ -3,27 +3,55 @@ import SortableColumnHeader from './SortableColumnHeader';
 import ProductRow from './ProductRow';
 
 export default class ProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSort = this.handleSort.bind(this);
+    this.state = {
+      sort: '',
+    };
+  }
+
+  handleSort(dir) {
+    console.log(dir);
+    this.setState({ sort: dir });
+  }
+
+
   render() {
 
     // Make array from object
-    let productsArray = [];
-    productsArray = Object.keys(this.props.products).map((pId) => this.props.products[pId]);
+    let pArr = [];
+    pArr = Object.keys(this.props.products).map((pId) => this.props.products[pId]);
+
+    pArr.sort((a, b) => {
+      if (!this.state.sort)
+        return ;
+      if (this.state.sort === 'desc')
+        return(a.name>b.name);
+      else {
+        return(a.name<b.name);
+      }
+    });
 
     let rows = [];
-    productsArray.forEach((product) => {
+    pArr.forEach((product) => {
       if (product.name.indexOf(this.props.search) === -1 || (this.props.inStockOnly && !product.stocked))
         return;
       return (
         rows.push(<ProductRow product={product} key={product.id} onClick={this.props.onClick}/>)
       );
-    })
+    });
 
     return (
       <div>
         <table>
           <thead>
             <tr>
-              <SortableColumnHeader column="name"/>
+              <SortableColumnHeader
+                onSort={this.handleSort}
+                currentSort={this.state.sort}
+                column="name"
+              />
               <SortableColumnHeader column="price"/>
             </tr>
           </thead>
--- product_list/src/components/SortableColumnHeader.js
+++ product_list/src/components/SortableColumnHeader.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 export default class SortableColumnHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.onSort = this.onSort.bind(this);
+  }
+
+  onSort(e) {
+    this.props.onSort(e.target.name);
+  }
+
   render() {
     return (
       <th>
         {this.props.column}
-        <button>&#x25B2;</button>
-        <button>&#x25BC;</button>
+        <button name='asc' onClick={this.onSort}>&#x25B2;</button>
+        <button name='desc' onClick={this.onSort}>&#x25BC;</button>
       </th>
     )
   }
