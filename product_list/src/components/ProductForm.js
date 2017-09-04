2d3cdbe84953f2d7a1ba640803196b3b7c9d689f
Natan Sabbah <nsabbah@student.42.fr>
2017-09-04 14:43:01 +0200

    error message if name blank

--- product_list/src/components/ProductForm.js
+++ product_list/src/components/ProductForm.js
@@ -6,7 +6,8 @@ export default class ProductForm extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        product: Object.assign({}, RESET_VALUES)
+        product: Object.assign({}, RESET_VALUES),
+        error: false
       }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -17,9 +18,18 @@ export default class ProductForm extends Component {
     let value = e.target[e.target.type === 'checkbox' ? 'checked' : 'value'];
     product[e.target.name] = value;
     this.setState({ product: product});
+    if (e.target.name === 'name') {
+      const error = this.state.product.name === '' ? true : false;
+      this.setState({ error });
+    }
   }
 
   handleSubmit(e) {
+    if (this.state.product.name === '') {
+      alert('No blank name');
+      e.preventDefault();
+      return;
+    }
     this.props.onSubmit(this.state.product);
     this.setState({
     product: Object.assign({}, RESET_VALUES)
@@ -29,23 +39,30 @@ export default class ProductForm extends Component {
 
 
   render() {
+
     return (
       <div>
         <h1>Enter New Product</h1>
         <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
           <label>
             Name:
-            <input type="text" name="name" value={this.state.product.name} required/>
+            <input type="text" name="name" value={this.state.product.name}/>
+            {
+              this.state.error &&
+              <div style={{color: 'red'}}>
+                Cannot be blank
+              </div>
+            }
           </label>
           <br/>
           <label>
             Category:
-            <input type="text" name="category" value={this.state.product.category} required/>
+            <input type="text" name="category" value={this.state.product.category}/>
           </label>
           <br/>
           <label>
             Price:
-            <input type="number" name="price" value={this.state.product.price} required/>
+            <input type="number" name="price" value={this.state.product.price}/>
           </label>
           <br/>
           <label>
